ed15598c0e03f36e4b7ddee63ccf7db002bf013a
Jannat <shahrinasabrin@gmail.com>
2022-11-11 23:57:22 +0600

    changes

--- my_website.js
+++ my_website.js
@@ -17,3 +17,14 @@ const textChange=()=>{
 }
 textChange();
 setInterval(textChange,8000);
+var acc = document.getElementsByClassName("accordion");
+
+  acc[0].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
